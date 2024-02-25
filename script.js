9ed67fba09794c11aee678abbc419ab045b8f389
sumandas473 <147833244+sumandas473@users.noreply.github.com>
2024-02-25 17:31:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let historyDiv = document.querySelector('.history');
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let history = '';
+    let string = "";
+
+    Array.from(buttons).forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            if(e.target.innerHTML == '='){
+                string=eval(string);
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'C'){
+                string="";
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'DEL'){
+                string = string.substring(0, string.length-1);
+                document.querySelector('input').value = string;
+            }
+            else{
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+
+            }
+
+           
+
+
+        })
+    })
+  
+
+
+
+})
